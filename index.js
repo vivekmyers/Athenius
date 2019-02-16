b6c6fceffd2827556ada873c1b94e305442a3605
Rohan Kalvade Deshpande <rohanvdeshpande@hotmail.com>
2019-02-16 14:54:25 -0800

    Integrated Legislator lookup api

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var express = require('express');
+const express = require('express');
+const request = require('request');
+const fs = require('fs');
+
+var keys = JSON.parse(fs.readFileSync('api-keys.json', 'utf8'));
 
 //App setup
 var app = express();
@@ -6,4 +10,22 @@ var server = app.listen(4000,function(){
 	console.log('listening to requests on port 4000');
 });
 
-app.use(express.static('public'));
+app.use(express.static(__dirname +'/public'));
+
+app.get('/leg-api', function(req, res){
+
+	var api_query = {
+		url: "https://q4ktfaysw3.execute-api.us-east-1.amazonaws.com/treehacks/legislators?address=450+Serra+Mall,+Stanford,+CA+94305&level=NATIONAL_LOWER",
+		headers:{
+			'x-api-key': keys["legislator-lookup-key"]
+		}
+	};
+
+	request(api_query, function(error, response, body){
+		if(!error && response.statusCode == 200){
+			const data = JSON.parse(body);
+			//console.log(data);
+			res.send(data);
+		}
+	});
+});
