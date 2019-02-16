63aa3c8f0e0731c258bab39d694a8904c2616ef0
Rohan Kalvade Deshpande <rohanvdeshpande@hotmail.com>
2019-02-16 00:19:45 -0800

    data-collection script pulls session data from senate.gov

--- pyscripts/data-collection.py
+++ pyscripts/data-collection.py
@@ -1,11 +1,28 @@
 import requests
+from xml.etree import ElementTree
 
 def constructSessionURL(senateNumber, sessionNumber):
-	return "https://www.senate.gov/legislative/LIS/roll_call_lists/vote_menu_" + senateNumber + "_" + sessionNumber + ".xml"
+	res =  "https://www.senate.gov/legislative/LIS/roll_call_lists/vote_menu_" + str(senateNumber) + "_" + str(sessionNumber) + ".xml"
+	print res
+	return res
+
+def outputSessionURL(senateNumber, sessionNumber):
+	return "data/Senate/Session/"+str(senateNumber)+"_"+str(sessionNumber)+".xml"
 
 def constructVoteURL(senateNumber, sessionNumber, voteNumber):
 	return "https://www.senate.gov/legislative/LIS/roll_call_votes/vote"+setnateNumber + sessionNumber + "/vote_" + senateNumber + "_" + sessionNumber + "_" + voteNumber + ".xml"
 
-response = requests.get(URL)
-with open('../data/vote_116_1_00023.xml', 'wb') as file:
-	file.write(response.content)
+
+
+for senateNumber in range(110, 117):
+	for sessionNumber in range(1, 3):
+		if not(senateNumber == 116 and sessionNumber == 2):
+
+			URL = constructSessionURL(senateNumber, sessionNumber)
+			response = requests.get(URL)
+			
+			with open(outputSessionURL(senateNumber, sessionNumber), 'wb') as file:
+				file.write(response.content)
+
+			tree = ElementTree.fromString(response.content)
+			
