96992050fc2d514b8c2f4eef7b0d70eb4da8e4aa
Rohan Kalvade Deshpande <rohanvdeshpande@hotmail.com>
2019-02-16 01:15:42 -0800

    data-procssing script pulls roll call votes. has error 503

--- pyscripts/data-collection.py
+++ pyscripts/data-collection.py
@@ -6,12 +6,16 @@ def constructSessionURL(senateNumber, sessionNumber):
 	print (res)
 	return res
 
-def outputSessionURL(senateNumber, sessionNumber):
+def outputSessionPath(senateNumber, sessionNumber):
 	return "data/Senate/Session/"+str(senateNumber)+"_"+str(sessionNumber)+".xml"
 
 def constructVoteURL(senateNumber, sessionNumber, voteNumber):
-	return "https://www.senate.gov/legislative/LIS/roll_call_votes/vote"+setnateNumber + sessionNumber + "/vote_" + senateNumber + "_" + sessionNumber + "_" + voteNumber + ".xml"
+	res = "https://www.senate.gov/legislative/LIS/roll_call_votes/vote"+str(senateNumber) + str(sessionNumber) + "/vote_" + str(senateNumber) + "_" + str(sessionNumber) + "_" + str(voteNumber).zfill(5) + ".xml"
+	print (res)
+	return res
 
+def outputVotePath(senateNumber, sessionNumber, voteNumber):
+	return "data/Senate/Votes/"+str(senateNumber)+"_"+str(sessionNumber)+"_"+str(voteNumber)+".xml"
 
 
 for senateNumber in range(110, 117):
@@ -23,15 +27,31 @@ for senateNumber in range(110, 117):
 			return_code = 503
 			attempt = 0
 			while return_code != 200 and attempt < 5:
-				response = requests.get(URL)
+				response = session.get(URL)
 				print (response)
 				return_code = response.status_code
 				attempt += 1
 
 			if return_code == 200:
-				with open(outputSessionURL(senateNumber, sessionNumber), 'wb') as file:
+				with open(outputSessionPath(senateNumber, sessionNumber), 'wb') as file:
 					file.write(response.content)
 
 				root = ElementTree.fromstring(response.content)
-				votes = len(root[3])
-				print (votes)
+				max_votes = len(root[3])
+				print (max_votes)
+
+				for voteNumber in range(1, max_votes+1):
+
+					URL = constructVoteURL(senateNumber, sessionNumber, voteNumber)
+
+					return_code = 503
+					attempt = 0
+					while return_code != 200 and attempt < 5:
+						response = session.get(URL)
+						print (response)
+						return_code = response.status_code
+						attempt += 1
+
+					if return_code == 200:
+						with open(outputVotePath(senateNumber, sessionNumber, voteNumber), 'wb') as file:
+							file.write(response.content)
