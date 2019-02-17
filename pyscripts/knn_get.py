75932ab2211a1e04d769fbe32fd3ad0328bde708
Vivek Myers <vivek.myers@gmail.com>
2019-02-17 08:28:42 -0800

    real real fix

--- pyscripts/knn_get.py
+++ pyscripts/knn_get.py
@@ -5,6 +5,11 @@ from random import shuffle
 from user_score import nearestNeighbors
 
 args = np.array([float(x) for x in sys.argv[1:]])
-for i in nearestNeighbors(args):
-    print(i[0][0])
+senate, house = nearestNeighbors(args)
+
+for i in senate:
+    print(i[0])
+
+for i in house:
+    print(i[0])
 
