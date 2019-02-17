08432ec46843986497ac04fab3e14f0e6e6c65aa
Vivek Myers <vivek.myers@gmail.com>
2019-02-16 23:50:45 -0800

    images from python queries

--- index.js
+++ index.js
@@ -18,22 +18,28 @@ var server = app.listen(4000, function () {
 
 app.use(express.static(__dirname + '/public'));
 
-app.post('/knn-get', function (req, res) {
+app.post('/knn/', function (req, res) {
     console.log(req.body);
     args = ['knn_get.py'];
-    for (let i of req.body) {
+    for (let i of req.body.vector) {
         args.push(i);
     }
-    console.log(args);
+    var arr = fs.readFileSync('image-links.csv').toString().split(/\n/g);
+    images = arr.map(i => i.split(/, /g)).map(v => [v[0].split(' ')[1], v[1]]);
+    image_dict = {};
+    for (let x of images)
+        image_dict[x[0]] = x[1];
     proc = spawn('python', args, {cwd: 'pyscripts'});
     proc.stdout.on('data', names => {
         names = names.toString();
-        console.log(names);
         output = {};
         for (let name of names.split(/\n/g)) {
-            if (name)
-                output[name] = 'https://kbam.net/mdbgui2.gif';
+            if (name) {
+                let url = image_dict[name.split(/ /g)[0].toLowerCase()];
+                output[name] = url ? url : 'congress.png';
+            }
         }
+        console.log(output);
         res.send(output);
     });
 });
--- pyscripts/knn_get.py
+++ pyscripts/knn_get.py
@@ -1,7 +1,9 @@
 import sys
 import data_loader
+from random import shuffle
 
 arr, reps, bills = data_loader.senate_records(1)
+shuffle(reps)
 
 for i in reps[:3]:
     print(i)
